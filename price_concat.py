bd352f2dc25b1c46e9fe896893210e52bb615b9a
tushar <tushar.mazumdar2013@gmail.com>
2025-03-28 12:46:29 +0530

    added text input to look for any stock.

--- price_concat.py
+++ price_concat.py
@@ -2,13 +2,14 @@ import yfinance as yf
 import pandas as pd
 import streamlit as st
 
-dat = yf.Ticker("TCS.NS")
+ticker = st.text_input('Enter Stock', '')
+dat = yf.Ticker(f'{ticker.upper()}.NS')
 
 def fetch_data():
     df = dat.history(period='1d', interval='1m')
     df.reset_index(inplace=True)
     st.line_chart(df.set_index('Datetime')['High'])
-    return df 
+    return df.tail()
 
 if st.button('Fetch Data'):
     st.write(fetch_data())
